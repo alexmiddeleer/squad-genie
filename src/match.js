4736ffe9c91298ad6c94e37609868f291bc17a8e
alexMA <almiddeleer@gmail.com>
2020-12-12 14:23:32 -0500

    Build team normalize and denormalizers

--- src/match.js
+++ src/match.js
@@ -6,13 +6,62 @@ import { cloneDeep } from "lodash";
 // example: [{id: 1, qtyNeeded: 1, prefs: [1,2,3]}, {id: 2, qtyNeeded: 2, prefs: [2,1,3]}]
 // example result (no offered swaps would be accepted):
 // [{id: 1, devIds:[1]}, {id: 2, devIds:[2,3]}]
+// idea: split team that needs N devs into N different teams and then join them at the end
 export default function match(devs, teams) {
-  devs = cloneDeep(devs);
+  const unmatched = cloneDeep(devs);
   teams = cloneDeep(teams);
 
-  // if (isStable(teams)) {
+  teams = normalizeTeams(teams);
+  teams.forEach(team => (team.dev = 1));
+  return denormalizeTeams(teams);
 
+  // while(unmatched.length) {
+  //   const dev = unmatched.pop();
+  //   dev.prefs.forEach(teamId => {
+  //     const team = teams.find(t => t.id === teamId);
+  //     if
+  //   })
   // }
 
   return cloneDeep([{}]);
+}
+
+const delimiter = "%%%";
+
+export function normalizeTeams(teams) {
+  return teams.reduce((acc, team) => {
+    if (team.qtyNeeded === 1) {
+      acc.push(team);
+      return acc;
+    }
+    for (let i = 0; i < team.qtyNeeded; i++) {
+      const subteam = cloneDeep(team);
+      subteam.id = `${team.id}${delimiter}${i + 1}`;
+      acc.push(subteam);
+    }
+    return acc;
+  }, []);
+}
+
+export function denormalizeTeams(teams) {
+  return teams.reduce((acc, team) => {
+    if (team.qtyNeeded === 1) {
+      team.devs = [team.dev];
+      delete team.dev;
+      acc.push(team);
+      return acc;
+    }
+    const teamId = team.id.split(delimiter).shift();
+    const i = acc.findIndex(t => t.id === teamId);
+    if (i > -1) {
+      acc[i].devs.push(team.dev);
+    } else {
+      const denormalizedTeam = cloneDeep(team);
+      denormalizedTeam.devs = [denormalizedTeam.dev];
+      delete denormalizedTeam.dev;
+      denormalizedTeam.id = teamId;
+      acc.push(denormalizedTeam);
+    }
+    return acc;
+  }, []);
 }
--- tests/unit/match.spec.js
+++ tests/unit/match.spec.js
@@ -1,7 +1,85 @@
-import match from "@/match.js";
+import match, { normalizeTeams, denormalizeTeams } from "@/match.js";
 
 describe("match", function() {
+  // it("should work", function() {
+  //   const devs = [{id: 1, prefs: [1,2]}, {id: 2, prefs: [2,1]}, {id: 3, prefs: [2,1]}];
+  //   const teams = [{id: 1, qtyNeeded: 1, prefs: [1,2,3]}, {id: 2, qtyNeeded: 2, prefs: [2,1,3]}]
+  //   const expected = [
+  //     {id: 1, devIds:[1], qtyNeeded: 1, prefs: [1,2,3]},
+  //     {id: 2, devIds:[2,3], qtyNeeded: 2, prefs: [2,1,3]}
+  //   ];
+  //   expect(match(devs, teams)).toEqual(expected);
+  // });
   it("should work", function() {
-    expect(match()).toEqual([{}]);
+    const teams = [
+      { id: "1", qtyNeeded: 1, prefs: [1, 2, 3] },
+      { id: "2", qtyNeeded: 2, prefs: [2, 1, 3] }
+    ];
+    const expected = [
+      { id: "1", devs: [1], qtyNeeded: 1, prefs: [1, 2, 3] },
+      { id: "2", devs: [1, 1], qtyNeeded: 2, prefs: [2, 1, 3] }
+    ];
+    expect(match(null, teams)).toEqual(expected);
+  });
+});
+
+describe("normalizeTeams", function() {
+  it("should return [] if input is []", function() {
+    expect(normalizeTeams([])).toEqual([]);
+  });
+  it("should echo back [{ id: 1, qtyNeeded: 1 }]", function() {
+    const input = [{ id: "1", qtyNeeded: 1 }];
+    expect(normalizeTeams(input)).toEqual(input);
+  });
+  it("should split a team [{ id: '1', qtyNeeded: 2 }]", function() {
+    const input = [{ id: "1", qtyNeeded: 2 }];
+    const expected = [
+      { id: "1%%%1", qtyNeeded: 2 },
+      { id: "1%%%2", qtyNeeded: 2 }
+    ];
+    expect(normalizeTeams(input)).toEqual(expected);
+  });
+  it("should split multiple teams", function() {
+    const input = [
+      { id: "1", qtyNeeded: 1 },
+      { id: "2", qtyNeeded: 2 }
+    ];
+    const expected = [
+      { id: "1", qtyNeeded: 1 },
+      { id: "2%%%1", qtyNeeded: 2 },
+      { id: "2%%%2", qtyNeeded: 2 }
+    ];
+    expect(normalizeTeams(input)).toEqual(expected);
+  });
+});
+
+describe("denormalizeTeams", function() {
+  it("should return [] if input is []", function() {
+    expect(denormalizeTeams([])).toEqual([]);
+  });
+  it("should listify the dev field", function() {
+    const input = [{ id: "1", qtyNeeded: 1, dev: 5 }];
+    const expected = [{ id: "1", qtyNeeded: 1, devs: [5] }];
+    expect(denormalizeTeams(input)).toEqual(expected);
+  });
+  it("should unsplit a team", function() {
+    const input = [
+      { id: "1%%%1", qtyNeeded: 2, dev: 1 },
+      { id: "1%%%2", qtyNeeded: 2, dev: 2 }
+    ];
+    const expected = [{ id: "1", qtyNeeded: 2, devs: [1, 2] }];
+    expect(denormalizeTeams(input)).toEqual(expected);
+  });
+  it("should unsplit multiple teams", function() {
+    const input = [
+      { id: "1", qtyNeeded: 1, dev: 1 },
+      { id: "2%%%1", qtyNeeded: 2, dev: 2 },
+      { id: "2%%%2", qtyNeeded: 2, dev: 3 }
+    ];
+    const expected = [
+      { id: "1", qtyNeeded: 1, devs: [1] },
+      { id: "2", qtyNeeded: 2, devs: [2, 3] }
+    ];
+    expect(denormalizeTeams(input)).toEqual(expected);
   });
 });
